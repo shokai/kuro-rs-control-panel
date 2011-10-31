2efd1745ffe257df417de797289822313ca4e552
Sho Hashimoto <hashimoto@shokai.org>
2011-11-01 02:59:12 +0900

    GET/POST /kuro-rs.json

--- helper.rb
+++ helper.rb
@@ -5,6 +5,7 @@ require 'rack'
 require 'sinatra/reloader' if development?
 require 'mongoid'
 require File.dirname(__FILE__)+'/models/ir'
+require 'net/http'
 require 'haml'
 require 'yaml'
 require 'json'
@@ -18,9 +19,15 @@ rescue => e
   exit 1
 end
 
+@@conf['kuro_rs_server'] += '/' unless @@conf['kuro_rs_server'] =~ /\/$/
+
 Mongoid.configure{|conf|
   conf.master = Mongo::Connection.new(@@conf['mongo_server'], @@conf['mongo_port']).db(@@conf['mongo_dbname'])
 }
+
+def kuro_rs_server
+  @@conf['kuro_rs_server']
+end
 
 def app_root
   "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
--- main.rb
+++ main.rb
@@ -2,7 +2,54 @@ before do
   @title = @@conf['title']
 end
 
+before '*.json' do
+  content_type = 'application/json'
+end
+
 get '/' do
   haml :index
 end
 
+get '/kuro-rs.json' do
+  uri = URI.parse kuro_rs_server
+  res = Net::HTTP.start(uri.host, uri.port).get(uri.path)
+  unless res.code == '200'
+    status 500
+    @mes = {:err => res.body}.to_json
+  else
+    status 200
+    @mes = {:data => res.body}.to_json
+  end
+end
+
+post '/kuro-rs.json' do
+  data = params[:data]
+  uri = URI.parse kuro_rs_server
+  res = Net::HTTP.start(uri.host, uri.port).post(uri.path, data)
+  unless res.code == '200'
+    status 500
+    @mes = {:err => res.body}.to_json
+  else
+    status 200
+    @mes = {:msg => res.body}.to_json
+  end
+end
+
+get '/ir/:name.json' do
+  @ir_name = params[:name]
+  @ir = IR.where(:name => @ir_name).first rescue @ir = nil
+  if @ir
+    @ir.to_hash.to_json
+  else
+    {:error => 'not found'}.to_json
+  end
+end
+
+post '/ir/:name.json' do
+  @ir_name = params[:name]
+end
+
+get '/ir/:name' do
+  @title = "#{@title} - IR:#{params[:name]}"
+  haml :ir
+end
